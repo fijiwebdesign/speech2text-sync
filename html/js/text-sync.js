9dcaa5f6bb0e3249b70f7525f861ee1651a787c0
Gabe L <gabe@fijiwebdesign.com>
2021-08-07 19:31:37 +0800

    plot max over time

--- html/js/text-sync.js
+++ html/js/text-sync.js
@@ -3,16 +3,15 @@ let audioContext;
 let audioBuffer;
 let sourceNode;
 let analyserNode;
-let javascriptNode;
-let audioData = null;
 let audioPlaying = false;
 let sampleSize = 256;  // number of samples to collect before analyzing data
 let amplitudeArray;     // array to hold time domain data
 // This must be hosted on the same server as this page - otherwise you get a Cross Site Scripting error
 let audioUrl = "audio/quote.webm";
 // Global variables for the Graphics
-let canvasWidth  = 512;
+let canvasWidth  = 1000;
 let canvasHeight = 256;
+let audio = null;
 
 // When the Start button is clicked, finish setting up the audio nodes, play the sound,
 // gather samples for the analysis, update the canvas
@@ -20,34 +19,33 @@ document.querySelector('#start_button').addEventListener('click', function(e) {
     // the AudioContext is the primary 'container' for all your audio node objects
     if(!audioContext) {
       try {
-          audioContext = new AudioContext();
+          audioContext = new (window.AudioContext || window.webkitAudioContext)();
       } catch(e) {
           alert('Web Audio API is not supported in this browser');
       }
     }
 
     e.preventDefault();
-    // Set up the audio Analyser, the Source Buffer and javascriptNode
-    setupAudioNodes();
 
     // Load the Audio the first time through, otherwise play it from the buffer
-    if(audioData == null) {
+    if(audio == null) {
         loadSound(audioUrl);
     } else {
-        playSound(audioData);
+        playSound(audio);
     }
 });
 
 // Stop the audio playing
 document.querySelector('#stop_button').addEventListener('click', function(e) {
     e.preventDefault();
-    sourceNode.stop(0);
+    audio.pause();
+    audio.currentTime = 0
     audioPlaying = false;
 });
 
 
-function setupAudioNodes() {
-    sourceNode     = audioContext.createBufferSource();
+function setupAudioNodes(stream) {
+    sourceNode = audioContext.createMediaStreamSource(stream);
     analyserNode   = audioContext.createAnalyser();
     //analyserNode.fftSize = sampleSize;
     // Create the array for the data values
@@ -60,31 +58,36 @@ function setupAudioNodes() {
 // Load the audio from the URL via Ajax and store it in global variable audioData
 // Note that the audio load is asynchronous
 function loadSound(url) {
-    document.getElementById('msg').textContent = "Loading audio...";
-    let request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    // When loaded, decode the data and play the sound
-    request.onload = function () {
-        audioContext.decodeAudioData(request.response, function (buffer) {
-            document.getElementById('msg').textContent = "Audio sample download finished";
-            audioData = buffer;
-            playSound(audioData);
-        }, onError);
-    }
-    request.send();
+
+  audio = new Audio(url);
+
+  document.getElementById('msg').textContent = "Loading audio...";
+
+  audio.addEventListener("canplaythrough", () => {
+    const stream = audio.captureStream();
+    setupAudioNodes(stream);
+    playSound(audio);
+  });
+  audio.addEventListener("timeupdate", () => {
+    console.log('time', { time: audio.currentTime, duration: audio.duration })
+  });
+  audio.addEventListener("ended", () => {
+    window.cancelAnimationFrame(drawCanvas);
+  });
 }
 
 // Play the audio and loop until stopped
-function playSound(buffer) {
-    sourceNode.buffer = buffer;
-    sourceNode.start(0);    // Play the sound now
-    sourceNode.loop = false;
+function playSound(audio) {
+    audio.play();
     audioPlaying = true;
 
-    setTimeout(() => audioPlaying = false, 5000)
+    setTimeout(() => {
+      audioPlaying = false
+
+      canvasDone()
+    }, 5000)
     
-    drawTimeDomain()
+    drawCanvas();
 }
 
 function onError(e) {
@@ -99,51 +102,65 @@ function createCanvas() {
   return canvas
 }
 
-let ampHistory = [];
-
 let canvas;
 const wordsEl = document.querySelector('#words')
 const wordsAudioEl = document.querySelector('#words-audio')
 const words = wordsEl.innerText.split(' ')
 
-function drawTimeDomain() {
+if (!canvas) canvas = createCanvas();
+ctx = canvas.getContext('2d');
+
+let x = 0;
+
+function drawCanvas() {
 
   if (!audioPlaying) return
 
-  requestAnimationFrame(drawTimeDomain)
+  requestAnimationFrame(drawCanvas)
 
   //analyserNode.getByteTimeDomainData(amplitudeArray);
   analyserNode.getByteFrequencyData(amplitudeArray);
-
-  if (!canvas) canvas = createCanvas();
-  ctx = canvas.getContext('2d');
   
-  ctx.beginPath();
   let totalAmp = 0;
   const len = amplitudeArray.length
   for (let i = 0; i < len; i++) {
       const y = amplitudeArray[i];
       let value = y / 256;
       let n = canvasHeight - (canvasHeight * value);
-      if (i === 0) {
-        ctx.moveTo(i, n);
-      } else {
-        ctx.lineTo(i, n);
-      }
+      // if (i === 0) {
+      //   ctx.moveTo(i, n);
+      // } else {
+      //   ctx.lineTo(i, n);
+      // }
       totalAmp += y;
   }
-  ctx.strokeStyle = '#000';
-  ctx.stroke();
-
-
+  
   const max = amplitudeArray.reduce((max, curr) => curr > max ? curr : max, 0)
   const min = amplitudeArray.reduce((min, curr) => curr < min ? curr : min, 0)
   const range = amplitudeArray.reduce((num, curr) => curr > 0 ? num+1 : num, 0)
-  const time = sourceNode.currentTime
+  const time = audio.currentTime
   const avg = Math.round(totalAmp/range)
-  console.log('stats', { totalAmp, avg, max, min, range, time })
-  ampHistory.push(totalAmp)
+  const ts = sourceNode.context.getOutputTimestamp()
+  console.log('stats', { x, ts, totalAmp, avg, max, min, range, time })
 
+  addLinePoint(x, max)
+  
+  x++;
+}
+
+function addLinePoint(x, y) {
+  if (x === 0) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+  }
+}
+
+function canvasDone() {
+  console.log('mapping lines')
+  ctx.strokeStyle = '#000';
+  ctx.stroke();
 }
 
 function clearCanvas() {
